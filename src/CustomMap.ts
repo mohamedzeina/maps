67f03d83909a1820f289b7254fd7dc0789cc3909
Mohamed Zeina <toxiczeina@gmail.com>
2024-07-01 16:50:15 +0300

    added interface mappable to refactor the duplicate code

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,13 @@
 import { Company } from './Company';
 import { User } from './User';
 
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -17,12 +24,12 @@ export class CustomMap {
     );
   }
 
-  setUserMarker(user: User) {
+  addMarker(mappable: Mappable) {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
